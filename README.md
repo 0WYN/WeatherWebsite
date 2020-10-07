632a80d1d2faa5c7e9ea0792045e1a7f02a3672a
Adam Halfpenny <adamhalfpenny@gmail.com>
2020-10-07 17:54:29 -0400

    Basic functions working

---
+++ README.md
@@ -0,0 +1,3 @@
+##WeatherWebsite##
+This website's purpose is so showcase and learn how to use APIs 
+API being used is from: https://openweathermap.org/
---
+++ script.js
@@ -0,0 +1,13 @@
+$.getJSON("http://api.openweathermap.org/data/2.5/weather?q=Toronto&units=metric&appid=85815ce2f4d68bcd25bbc445d9f822c6", function(data){
+    console.log(data);
+    var icon = 'http://openweathermap.org/img/w/' + data.weather[0].icon + '.png';
+    var temp = Math.floor(data.main.temp) + '°C';
+    var feels_like = Math.floor(data.main.feels_like) + '°C';
+    var weather = data.weather[0].main;
+
+    $('.weather').append(weather);
+    $('.icon').attr('src', icon);
+    $('.temp').append(temp);
+    $('.feels_like').append(feels_like);
+});
+
